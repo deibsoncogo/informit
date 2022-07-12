0e2d5a696605434320bc424c8d79bcb9cf998c67
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-11 21:40:53 -0300

    ✨ feat: creation component header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,18 @@
+import { UseMessage } from '../../contexts/messageContext'
+import style from './style.module.scss'
+
+export function Header() {
+  const { message } = UseMessage()
+
+  return (
+    <div className={style.header}>
+      <h1 className={style.title}>
+        Informit
+
+        <strong id={message?.isError && style.error} className={style.message}>
+          {message?.description}
+        </strong>
+      </h1>
+    </div>
+  )
+}
