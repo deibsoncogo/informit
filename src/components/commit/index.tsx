42fb74f16b7cad43abb40136535e5a082dba32bf
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-14 08:15:29 -0300

    ✨ feat: creation component commit

---
+++ src/components/commit/index.tsx
@@ -0,0 +1,67 @@
+import style from './style.module.scss'
+
+type TProps = {
+  commitMessage: string
+  commitUrl: string
+  commitDate: string
+  authorName: string
+  authorLogin: string
+  authorAvatarUrl: string
+  authorHtmlUrl: string
+  repositoryFullName: string
+  repositoryName: string
+  repositoryDescription: string
+  repositoryOwnerType: string
+}
+
+export function Commit({
+  commitMessage, commitUrl, commitDate, authorName, authorLogin, authorAvatarUrl, authorHtmlUrl,
+  repositoryFullName, repositoryName, repositoryDescription, repositoryOwnerType,
+}: TProps) {
+  const urlRepository = `https://github.com/${repositoryFullName}`
+  const newDate = new Date(commitDate)
+
+  return (
+    <div className={style.commit}>
+      <section className={style.commitNote}>
+        <strong>{commitMessage}</strong>
+
+        <a style={{ width: 100 }} href={commitUrl} target='_blank'>
+          <p>{commitUrl}</p>
+        </a>
+      </section>
+
+      <section className={style.commitExtra}>
+        <span className={style.information}>
+          <a href={urlRepository} target='_blank' title={repositoryDescription} id={repositoryOwnerType}>
+            <strong className={style.repositoryName}>{repositoryName}</strong>
+          </a>
+
+          <a href={authorHtmlUrl} target='_blank' title={`login: ${authorLogin}`}>
+            <strong className={style.authorName}>{authorName}</strong>
+          </a>
+        </span>
+
+        <span className={style.dateTime}>
+          <strong className={style.time} title={newDate.toTimeString()}>
+            {newDate.toLocaleTimeString()}
+          </strong>
+
+          <strong
+            className={style.date}
+            title={newDate.toLocaleDateString(
+              [],
+              { weekday: 'long', day: '2-digit', month: 'long', year: 'numeric' },
+            )}
+          >
+            {newDate.toLocaleDateString()}
+          </strong>
+        </span>
+
+        <a href={authorHtmlUrl} target='_blank'>
+          <img src={authorAvatarUrl} alt='Avatar' className={style.avatar} />
+        </a>
+      </section>
+    </div>
+  )
+}
