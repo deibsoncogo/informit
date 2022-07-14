630013e4b667e3b3f55d86473cb471a7b9ce869b
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-14 09:00:51 -0300

    ✨ feat: creation message contato

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
+import { useEffect } from 'react'
 import { Header } from '../components/header'
 import { ListCommit } from '../components/listCommit'
 import { Search } from '../components/search'
+import { UseMessage } from '../contexts/messageContext'
 import style from './index.module.scss'
 
 export default function Index() {
+  const { CreateMessage } = UseMessage()
+
+  useEffect(() => {
+    setTimeout(() => {
+      CreateMessage({
+        description: `
+        Achou algum erro ou quer melhorar algo,
+        então entre em contato deibsoncogo@outlook.com   //   (16) 99708-4454
+        `,
+        isError: false,
+      })
+    }, 1000 * 60 * 30)
+  }, [CreateMessage])
+
   return (
     <div className={style.index}>
       <title>Informit</title>
