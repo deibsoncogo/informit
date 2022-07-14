32e7bc2ebb6aab970ad6e8d77fc8d6ddad7554cd
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-14 00:39:25 -0300

    ♻️ feat: implementation import componentes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import { Header } from '../components/header'
+import { ListCommit } from '../components/listCommit'
+import { Search } from '../components/search'
 import style from './index.module.scss'
 
 export default function Index() {
   return (
     <div className={style.index}>
-      Index
+      <Header />
+
+      <main>
+        <Search />
+        <ListCommit />
+      </main>
+
+      <footer />
     </div>
   )
 }
